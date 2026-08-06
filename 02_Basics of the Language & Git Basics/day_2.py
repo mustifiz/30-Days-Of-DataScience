a3f18f2c8d98704876dd6abcd1a73ebe647425b6
musticodes <mustafaoztemiz27@gmail.com>
2026-08-06 11:09:52 +0200

    Add Day 2 Python practice

---
+++ 02_Basics of the Language & Git Basics/day_2.py
@@ -0,0 +1,48 @@
+#### Example: Basic Python Syntax  
+
+# Dies ist ein einzeiliger Kommentar.
+print("Hello ,Data Science!") # # Dies gibt eine Meldung auf der Konsole aus
+
+
+# Indentation defines blocks of code in Python. For example, the following code block is indented to indicate that it is part of the if statement:
+
+if 5 > 2:
+    print("Five is greater than two!") # Correct indentation is crucial in Python, as it defines the structure and flow of the code.
+
+
+ ### 2️⃣ Variables in Python 🛠  
+
+ # Assigning values to variables is straightforward in Python. You can use the assignment operator (=) to assign a value to a variable.
+
+name = "Alice"
+age = 30
+height = 5.7
+
+# Printing the values of the variables
+print(name)
+print(age)
+print(height)
+
+
+# String
+greeting = "Hello, World!"
+
+# Integer
+year = 2024
+
+# Float
+pi = 3.14159
+
+# Boolean
+is_active = True
+
+print(greeting)  # Output: Hello, World!
+print(year)      # Output: 2024
+print(pi)        # Output: 3.14159
+print(is_active) # Output: True
+
+
+
+count = 10
+count += 1  # Increment by 1
+print(count)  # Output: 11
